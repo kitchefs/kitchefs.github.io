5f53417c2abc91ec2159ac74a7df23edeb31e583
SimpleBinary <simplebinary@yahoo.com>
2020-10-09 10:52:53 +0000

    Recipe

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,66 @@
 <script>
-    import Navbar from "./components/navbar.svelte"
+    import Navbar from "./components/navbar.svelte";
+    import Recipe from "./components/recipe.svelte";
 </script>
+
+<style>
+    .container {
+        padding-top: 80px;
+    }
+</style>
 
 <svelte:head>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link 
+        rel="stylesheet" 
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+        crossorigin="anonymous">
 
     <!-- Bootstrap JS -->
-    <script defer src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script 
+        defer
+        src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous">
+    </script>
+    <script
+        defer
+        src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous">
+    </script>
+    <script
+        defer
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous">
+    </script>
 </svelte:head>
 
-<Navbar items={[
-    {
-        name: "Home",
-        href: "./index.html",
-        active: true,
-    },
-    {
-        name: "Epic",
-        href: "#",
-        active: false,
-    }
-]}/>
+<Navbar
+    items={[{ name: 'Home', href: './index.html', active: true }, { name: 'Epic', href: '#', active: false }]} />
+
+<div class="container">
+    <div class="row">
+        <div class="row mb-2">
+            <Recipe
+                name={'Hot Cross Buns'}
+                image_src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Hot_cross_buns_-_fig_and_pecan.jpg/1200px-Hot_cross_buns_-_fig_and_pecan.jpg'}
+                description={'A perfect Easter snack for the whole family.'}
+                href={'#'} />
+            
+            <Recipe
+                name={'Blueberry Muffins'}
+                image_src={'https://i2.wp.com/smittenkitchen.com/wp-content/uploads//2010/08/perfect-blueberry-muffins.jpg'}
+                description={'A sweet treat famous for its aroma.'}
+                href={'#'} />
+            
+            <Recipe
+                name={'Caramel Slice'}
+                image_src={'https://www.sweetestmenu.com/wp-content/uploads/2020/04/caramelslice2a.jpg'}
+                description={'Perfect for any sweet-tooth, this Caramal Slice is bound to turn a few heads!'}
+                href={'#'} />
+        </div>
+    </div>
+</div>
---
+++ src/components/recipe.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let name;
+    export let image_src;
+    export let description;
+    export let href;
+</script>
+
+<div class="col-md-4">
+    <div onclick="window.location='{href}'" class="card mb-4 shadow-sm">
+        <img src="{image_src}" width="100%" height="200px" alt="{name}">
+        <div class="card-body">
+            <h3 class="card-text">{name}</h3>
+            <p class="card-text">{description}</p>
+        </div>
+    </div>
+</div>
+
+<style>
+    img {
+        object-fit: cover;
+    }
+</style>
