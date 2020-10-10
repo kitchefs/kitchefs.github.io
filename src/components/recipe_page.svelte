c39919b5b873ff30340c399f05deac4bca401664
Kitchefs <62424502+Kitchefs@users.noreply.github.com>
2020-10-10 22:36:59 +1000

    added recipe_page

---
+++ src/components/recipe_page.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let name;
+    export let description;
+    export let image;
+    export let ingredients;
+    export let steps;
+    export let tips;
+</script>
+
+<div class="container">
+    <h3>{name}</h3>
+    <p>{description}</p>
+    <img src={image} alt={name} />
+
+    {#each ingredients as item}
+        <p>{item}</p>
+    {/each}
+
+    {#each steps as step}
+        <p>{step}</p>
+    {/each}
+
+    {#each tips as tip}
+        <p>{tip}</p>
+    {/each}
+</div>
