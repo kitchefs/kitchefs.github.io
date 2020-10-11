b197132447607d5c939b9156ac09a5ca7762d924
SimpleBinary <simplebinary@yahoo.com>
2020-10-11 22:10:29 +0000

    Fix Recipe Card

--- src/components/recipe.svelte
+++ src/components/recipe.svelte
@@ -18,11 +18,15 @@
     }
     h3 {
         font-family: "Playfair Display";
+        font-size: 150%;
+    }
+    .item-card {
+        height: 370px;
     }
 </style>
 
 <div class="col-md-4" item-name="{lowercase(name)}" category-name="{lowercase(category)}">
-    <div onclick="window.location='{href}'" class="card mb-4 shadow-sm">
+    <div onclick="window.location='{href}'" class="card mb-4 shadow-sm item-card">
         <img src={image} width="100%" height="200px" alt={name} />
         <div class="card-body">
             <h3 class="card-text">{name}</h3>
