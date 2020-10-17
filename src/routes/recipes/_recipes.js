85b30caa824e767bb040f8ff1d8315937b526c13
Kitchefs <62424502+Kitchefs@users.noreply.github.com>
2020-10-17 15:47:08 +1000

    update

--- src/routes/recipes/_recipes.js
+++ src/routes/recipes/_recipes.js
@@ -8,9 +8,9 @@ const recipes = [
 		ingredients: ['3 ¼ Cups Plain Flour',' 1 Cup Sultanas',' 2 Tbs Orange Peel',' 2 Tbs Caster Sugar',' 1 Tsp Dried Yeast',' 1 Tsp Mixed Spice',' 1 Tsp Ground Cinnamon',' 1 Tsp Ground Nutmeg',' 1 Tsp Salt',' 1 Cup Warm Milk',' 50g Butter, Melted',' 1 Egg, Whisked'],
 		steps: ['Line baking tray with baking paper','In a large bowl combine Flour, Sultanas, Orange Peel, Sugar, Yeast, Spices, Nutmeg and salt.','Make a well in the center ad mix in Milk, Butter and Egg. Use your hands to bring the dough together in the bowl.','Turn the dough onto a floured surface and knead for 10-15 minutes or until smooth.','Coat with Melted Butter and leave to rise for 1 ½ hours.','Preheat oven to 180°C. knead dough for 3-5 minutes, until smooth.','Divide into 12 balls, Place onto prepared tray. Set aside for 30 minutes.','For the Cross Paste, mix ¼ Cup Flour and 2 Tbs water. Transfer into piping bag and make Crosses','Bake for 20-25 minutes, Leave to cool.'],   
 		tips: ['You can use any citrus peel instead of Orange Peel'],
-   		preparation_time: '35 Minutes',
-   		cook_time: '20 Minutes',
-		total_time: '6 Hours, 5 Minutes',
+   		preparation_time: '30 Minutes',
+   		cook_time: '25 Minutes',
+		total_time: '2 Hours, 55 Minutes',
 		featured: true
 	},
 	{
@@ -26,6 +26,20 @@ const recipes = [
    		cook_time: '69 Minutes',
 		total_time: '1 Hour, 5 Minutes',
 		featured: true
+	},	
+	{
+		name: 'Chocolate Caramel Slice',
+		slug: 'Chocolate-Caramel-Slice',
+		description: 'Awesome Dessert Slice',
+		category: 'Dessert',
+		image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Hot_cross_buns_-_fig_and_pecan.jpg/1200px-Hot_cross_buns_-_fig_and_pecan.jpg',
+		ingredients: ['1 Cup Plain Flour','1 Cup Brown Sugar','½ Desiccated Coconut','125g Butter, Melted','100g Butter, Extra','2 Cans Condensed Milk','⅓ Golden Syrup','200g Dark Chocolate','1 Tbsp Vegtable Oil'],
+		steps: ['Line a 20cm x 10cm tray, preheat oven to 180°C.','In a medium sized bowl, combine Flour, Sugar and Coconut.','Add in Melted Butter, mix will.','Press mixture firmly into pan, Bake for 15-20 minutes or until brown. Leave to cool.','Place Extra Butter, Condensed Milk and Golden Syrup in a medium sized saucepan. Stir over low heat until smooth.','Pour over base. Bake For 20-25 minutes or until golden. Leave to cool','Combine Chocolate and Oil, stir until smooth, pour evenly over slice. Refrigerate for 5-6 hours.'],   
+		tips: ['This is an example.'],
+   		preparation_time: '30 Minutes',
+   		cook_time: '45 Minutes',
+		total_time: '6 Hours, 15 Minutes',
+		featured: false
 	}
 ];
 
