42a9acbe4fdae9740ed6ea5437e9adceb64faaa7
SimpleBinary <simplebinary@yahoo.com>
2020-10-18 20:25:16 +1000

    [Broken} Upload for saving progress

--- src/routes/saved_items.svelte
+++ src/routes/saved_items.svelte
@@ -1,5 +1,33 @@
+<script context="module">
+	import { onMount }  from 'svelte';
+    import Item         from "../components/item.svelte";
+
+	export function preload() {
+		return this.fetch(`recipes.json`).then(r => r.json()).then(recipes => {
+			return { recipes };
+		});
+	}
+</script>
+
+<script>
+    export let recipes;
+    
+	let saved_items = [];
+	
+	function set_saved_items(x) {
+		saved_items = x;
+	}
+    
+    onMount(() => {
+		if (localStorage.hasOwnProperty("kitchefs_saved_items")) {
+			set_saved_items(localStorage.getItem("kitchefs_saved_items"));
+		}
+	});
+
+</script>
+
 <svelte:head>
-	<title>Saved Items</title>
+	<title>Kitchefs | Saved Items</title>
 </svelte:head>
 
 <style>
@@ -8,6 +36,21 @@
 	}
 </style>
 
-<h1>Saved Recipes</h1>
+<h1>Saved Items</h1>
 
-<p>There's not much here.</p>
+<div class="container">
+    <div class="row">
+		{#each saved_items as saved_item}
+            {#each recipes as recipe}
+				{#if saved_item == recipe.slug}
+					<Item
+					name={recipe.name}
+					category={recipe.category}
+					image={recipe.image}
+					description={recipe.description}
+					href={'recipes/' + recipe.slug} />
+				{/if}
+			{/each}
+        {/each}
+	</div>
+</div>
