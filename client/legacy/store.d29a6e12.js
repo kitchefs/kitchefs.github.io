a5162d5c2b77f39948741375635bf09e1c45b100
SimpleBinary <simplebinary@yahoo.com>
2020-10-24 04:52:11 +0000

    Update gh-pages to output generated at ef20551

---
+++ client/legacy/store.d29a6e12.js
@@ -0,0 +1,1 @@
+import{G as e}from"./client.41cac1ca.js";var t=e([]);"undefined"!=typeof window&&(null===localStorage.getItem("kitchefs")&&localStorage.setItem("kitchefs",JSON.stringify([])),(t=e(JSON.parse(localStorage.getItem("kitchefs")))).subscribe((function(e){return localStorage.setItem("kitchefs",JSON.stringify(e))})));export{t as s};
